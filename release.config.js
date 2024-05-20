598ae87ba04178e6bdfd3e093d32eb81c2a122fc
YAMADA Naoki <wakamsha@gmail.com>
2024-05-21 02:37:00 +0900

    ci: Implement Release workflow (#3)
    * specify access prop as `public`
    
    * install node packages
    
    * implement release config
    
    * implement gha workflow

---
+++ release.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
