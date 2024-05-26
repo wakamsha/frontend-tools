ab4849f60768d7257722d700eba393d244221d5c
YAMADA Naoki <wakamsha@gmail.com>
2024-05-26 16:56:40 +0900

    feat: Implement the rule "react-hooks" (#21)

---
+++ rules/react-hooks.js
@@ -0,0 +1,18 @@
+import { defineFlatConfig } from 'eslint-define-config';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default defineFlatConfig({
+  plugins: {
+    'react-hooks': reactHooks,
+  },
+
+  rules: {
+    // Verify the list of the dependencies for Hooks like useEffect and similar.
+    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/ExhaustiveDeps.js
+    'react-hooks/exhaustive-deps': ['error'],
+
+    // Enforce Rules of Hooks.
+    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/src/RulesOfHooks.js
+    'react-hooks/rules-of-hooks': ['error'],
+  },
+});
