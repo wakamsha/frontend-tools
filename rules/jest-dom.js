fe0fa6ba836c5f7192dd7ad7efa582a85e643931
YAMADA Naoki <wakamsha@gmail.com>
2024-05-26 20:50:25 +0900

    feat: Implement the rule "jest-dom" (#35)

---
+++ rules/jest-dom.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineFlatConfig } from 'eslint-define-config';
+import jestDom from 'eslint-plugin-jest-dom';
+
+export default defineFlatConfig({
+  plugins: {
+    'jest-dom': jestDom,
+  },
+
+  rules: {
+    ...jestDom.configs.recommended.rules,
+  },
+});
