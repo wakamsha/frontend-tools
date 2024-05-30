2c5bfed4735697e493a5f79890e344d55963ac0c
YAMADA Naoki <wakamsha@gmail.com>
2024-05-30 19:51:04 +0900

    fix: `import/namespace` error (#44)
    * fix: import/namespace error
    
    * chore: Implement linting source code
    
    * chore: Implement linting workflow

--- rules/imports.js
+++ rules/imports.js
@@ -24,7 +24,7 @@ export default {
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.mjs', '.js', '.json'],
+        extensions: ['.mjs', '.js', '.json', '.ts'],
       },
       // Resolve the problem of incorrect recognition of alias paths by TypeScript compiler options.
       // https://github.com/import-js/eslint-plugin-import/issues/1485#issuecomment-535351922
@@ -36,6 +36,11 @@ export default {
       'node_modules',
       '\\.(coffee|scss|css|less|hbs|svg|json|jpg|jpeg|png|webp)$',
     ],
+    // TODO: Remove this once eslint-plugin-import supports Flat Config completely.
+    // https://github.com/import-js/eslint-plugin-import/issues/2556#issuecomment-1419518561
+    'import/parsers': {
+      espree: ['.js', '.mjs', '.jsx', 'ts', 'tsx'],
+    },
   },
 
   rules: {
