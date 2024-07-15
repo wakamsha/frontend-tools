ac266e0a73dbceb2c9dfb1fc96860d2d22f4bdab
wakamsha <wakamsha@gmail.com>
2024-07-15 13:44:13 +0900

    update snapshot

--- configs/essentials.js
+++ configs/essentials.js
@@ -1,10 +1,12 @@
 // @ts-check
+import globals from 'globals';
 import bestPracticesRuleSet from '../rules/best-practices.js';
 import errorsRuleSet from '../rules/errors.js';
 import es6RuleSet from '../rules/es6.js';
 import importsRuleSet from '../rules/imports.js';
 import promiseRuleSet from '../rules/promise.js';
 import styleRuleSet from '../rules/style.js';
+import unicornRuleSet from '../rules/unicorn.js';
 import variablesRuleSet from '../rules/variables.js';
 
 export default [
@@ -14,6 +16,7 @@ export default [
         ecmaVersion: 2023,
         sourceType: 'module',
       },
+      globals: globals.builtin,
     },
   },
 
@@ -24,4 +27,10 @@ export default [
   variablesRuleSet,
   importsRuleSet,
   promiseRuleSet,
+
+  unicornRuleSet.basic,
+  {
+    files: ['**/*.@(jsx|tsx)'],
+    ...unicornRuleSet.forReact,
+  },
 ];
