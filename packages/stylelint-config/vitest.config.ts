89e598fb8d4887914becb1383cfe57bc3d662dfb
wakamsha <wakamsha@gmail.com>
2024-10-30 18:27:47 +0900

    fix ci

--- packages/stylelint-config/vitest.config.ts
+++ packages/stylelint-config/vitest.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineProject } from 'vitest/config';
 
-export default defineConfig({
+export default defineProject({
   test: {
     globals: true,
     include: ['test/**/*.test.ts'],
-    exclude: ['test/**/dummy.test.ts'],
   },
 });
