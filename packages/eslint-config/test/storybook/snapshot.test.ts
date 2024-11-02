208d4e81fef8d1296e51d13e997c5d31ef7d7e7f
YAMADA Naoki <wakamsha@gmail.com>
2024-11-02 21:09:36 +0900

    fix: `tsconfigRootDir` specification (#125)
    * fix: `tsconfigRootDir` specification
    
    * update snapshot
    
    * update snapshot
    
    * update snapshot
    
    * fix

--- packages/eslint-config/test/storybook/snapshot.test.ts
+++ packages/eslint-config/test/storybook/snapshot.test.ts
@@ -1,9 +1,19 @@
 import { getESLintConfig } from '../helper.ts';
 
-test('should match ESLint Configuration snapshot: storybook', async () => {
-  const filePath = 'dummy.stories.tsx';
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const config = await getESLintConfig(filePath, import.meta.dirname);
+describe('ESLint Configuration Snapshot Tests', () => {
+  beforeAll(() => {
+    process.env.TSCONFIG_ROOT_DIR = '/dummy';
+  });
 
-  expect(config).toMatchSnapshot();
+  test('should match ESLint Configuration snapshot: storybook', async () => {
+    const filePath = 'dummy.stories.tsx';
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const config = await getESLintConfig(filePath, import.meta.dirname);
+
+    expect(config).toMatchSnapshot();
+  });
+
+  afterAll(() => {
+    process.env.TSCONFIG_ROOT_DIR = undefined;
+  });
 });
