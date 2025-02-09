83ecd02924c50ecfb66c7dcbd128e3c01412e991
wakamsha <wakamsha@gmail.com>
2025-02-09 21:46:48 +0900

    fix(deps): Remove stylelint-config-recommended

--- packages/stylelint-config/configs/essentials.js
+++ packages/stylelint-config/configs/essentials.js
@@ -1,27 +1,23 @@
 export default {
-  extends: [
-    "stylelint-config-recommended",
-    "stylelint-config-standard",
-    "stylelint-config-recess-order",
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
 
   rules: {
-    "declaration-block-no-redundant-longhand-properties": [
+    'declaration-block-no-redundant-longhand-properties': [
       true,
       {
-        ignoreShorthands: ["grid-template"],
+        ignoreShorthands: ['grid-template'],
       },
     ],
-    "property-no-vendor-prefix": [
+    'property-no-vendor-prefix': [
       true,
       {
-        ignoreProperties: ["text-size-adjust", "background-clip"],
+        ignoreProperties: ['text-size-adjust', 'background-clip'],
       },
     ],
-    "selector-pseudo-class-no-unknown": [
+    'selector-pseudo-class-no-unknown': [
       true,
       {
-        ignorePseudoClasses: ["global"],
+        ignorePseudoClasses: ['global'],
       },
     ],
   },
