9d6805c776c371f35806f54805ae32ba7ce7e359
wakamsha <wakamsha@gmail.com>
2026-06-08 22:00:49 +0900

    chore: update engines

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -3,21 +3,20 @@ import { essentials, node, test, typescript } from './configs/index.js';
 
 export default [
   {
-    files: ['**/*.@(js|ts)'],
+    files: ['**/*.{js,ts}'],
   },
 
   ...essentials,
+  ...node,
+  ...test.essentials,
+  ...typescript,
+
   {
-    files: ['**/*.@(js|ts)'],
+    files: ['**/*.{js,ts}'],
     rules: {
       'import/no-default-export': ['off'],
+      'n/no-unsupported-features/node-builtins': ['off'],
       'unicorn/filename-case': ['off'],
     },
   },
-
-  ...node,
-
-  ...test.essentials,
-
-  ...typescript,
 ];
